f099a0feb7210d439e90f9743e16f2b5f9a1e072
Cole Striler <colestriler@gmail.com>
2020-09-04 13:54:43 -0700

    done

--- flaskapp/bots/app.py
+++ flaskapp/bots/app.py
@@ -1,6 +1,29 @@
 from flask import render_template, request, Blueprint, flash, redirect, url_for
+from apscheduler.schedulers.blocking import BlockingScheduler
+from flaskapp.bots.create_tweet import tweet
 
 bots = Blueprint('bots', __name__)
 
 
+sched = BlockingScheduler()
 
+# @sched.scheduled_job('interval', minutes=.5)
+# def timed_job():
+#     print('This job is run every three minutes.')
+#     api.update_status("hello")
+
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=13, minute=56)
+def test():
+    tweet()
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=6)
+def morning():
+    tweet()
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=16)
+def afternoon():
+    tweet()
+
+
+sched.start()
---
+++ schedule.py
@@ -0,0 +1,27 @@
+# from apscheduler.schedulers.blocking import BlockingScheduler
+# from flaskapp.bots.create_tweet import tweet
+#
+#
+#
+# sched = BlockingScheduler()
+#
+# # @sched.scheduled_job('interval', minutes=.5)
+# # def timed_job():
+# #     print('This job is run every three minutes.')
+# #     api.update_status("hello")
+#
+#
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=13, minute=51)
+# def test():
+#     tweet()
+#
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=6)
+# def morning():
+#     tweet()
+#
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=16)
+# def afternoon():
+#     tweet()
+#
+#
+# sched.start()
