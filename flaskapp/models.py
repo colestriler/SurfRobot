03162b39c1b586cecc8ed777ddc36a212e1b5720
Cole Striler <colestriler@gmail.com>
2020-09-15 17:54:00 -0700

    change print model

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -12,6 +12,8 @@ class Report(db.Model):
     swells = db.Column(db.String(), nullable=True)
     weather = db.Column(db.String(), nullable=False)
     h20_temp = db.Column(db.String(), nullable=False)
+    # first_light = db.Column(db.DateTime, nullable=True)
+    # last_light = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
-        return f"Report('{self.location}', '{self.condition}', '{self.date}')"
+        return f"Report('{self.id}', '{self.date}', '{self.location}', '{self.condition}', '{self.wave_height}', '{self.tide}', '{self.wind}', '{self.swells}', '{self.weather}', '{self.h20_temp}')"
